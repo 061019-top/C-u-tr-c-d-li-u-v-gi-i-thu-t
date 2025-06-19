4146f778a790be195537491ae7e439b02210b659
061019-top <chudinhminh21@gmail.com>
2025-06-19 22:17:39 +0700

    update btvn ss2

--- Session02/PTIT_CNTT3_IT104_Session02_Bai02.c
+++ Session02/PTIT_CNTT3_IT104_Session02_Bai02.c
@@ -1,3 +1,44 @@
-//
-// Created by ADMIN on 6/17/2025.
-//
+#include <stdio.h>
+
+int count(int arr[], int len, int val) {
+    int c = 0;
+    for (int i = 0; i < len; i++) {
+        if (arr[i] == val) {
+            c++;
+        }
+    }
+    return c;
+}
+
+int main() {
+    int n;
+    int x;
+
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+
+    if (n <= 0 || n > 100) {
+        printf("Phan tu khong hop le\n");
+        return 0;
+    }
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        printf("Phan tu thu %d: ", i + 1);
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d, ", a[i]);
+    }
+    printf("\n");
+
+    printf("Nhap mot so bat ky: ");
+    scanf("%d", &x);
+
+    int r = count(a, n, x);
+    printf("%d", r);
+
+    return 0;
+}
+
